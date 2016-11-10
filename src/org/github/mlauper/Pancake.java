31e29eae6fe7dfc691b9fbbbf8180becf1117bdd
mLauper <laupm4@bfh.ch>
2016-11-10 19:38:41 +0100

    Change Pancake

--- src/org/github/mlauper/Pancake.java
+++ src/org/github/mlauper/Pancake.java
@@ -1,7 +1,73 @@
 package github.mlauper;
 
-/**
- * Created by Marco on 10.11.2016.
- */
-public class PanCake {
+import java.util.LinkedList;
+
+public class Pancake {
+    private LinkedList<Integer> pancakes;
+    private LinkedList<LinkedList<Integer>> candidatePancakes = new LinkedList<>();
+    private LinkedList<LinkedList<Integer>> failedPancakes = new LinkedList<>();
+
+    public Pancake(LinkedList<Integer> pancakes){
+        this.pancakes = pancakes;
+    }
+
+    public void printPancakes (){
+        System.out.println(pancakes);
+    }
+
+    public void calculateFlips () {
+        while(!this.isPancakeOrdered()){
+            failedPancakes.add((LinkedList<Integer>) pancakes.clone());
+            for (LinkedList<Integer> pancakeCandidate :
+                    this.getChildStates()) {
+                if(!candidatePancakes.contains(pancakeCandidate) && !failedPancakes.contains(pancakeCandidate)){
+                    candidatePancakes.add(pancakeCandidate);
+                }
+            }
+
+            this.pancakes = candidatePancakes.pop();
+        }
+    }
+
+    public boolean isPancakeOrdered(){
+        int size = pancakes.size();
+        for (int i = 0; i < size-1; i++) {
+            if (pancakes.get(i) + 1 != pancakes.get(i + 1)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void flip(int Position){
+        pancakes = this.fliped(Position);
+    }
+
+    public LinkedList<Integer> fliped(int Position){
+        LinkedList<Integer> flipedPancakes = (LinkedList<Integer>) pancakes.clone();
+        LinkedList<Integer> temp = new LinkedList<>();
+        int j = 0;
+        for (int i = Position; i < flipedPancakes.size(); i++){
+            temp.add(j,flipedPancakes.get(i));
+            j++;
+        }
+        j = Position;
+        for (int i = temp.size()-1; i >= 0; i--){
+            flipedPancakes.set(j, temp.get(i));
+            j++;
+        }
+        return flipedPancakes;
+    }
+
+    public LinkedList<Integer> getPancakes(){
+        return pancakes;
+    }
+
+    public LinkedList<LinkedList<Integer>> getChildStates() {
+        LinkedList<LinkedList<Integer>> childStates = new LinkedList<>();
+        for (int i = 1; i < pancakes.size()-1; i++) {
+            childStates.add(new LinkedList<>(this.fliped(i)));
+        }
+        return childStates;
+    }
 }
--- src/test/org/github/mlauper/PancakeTest.java
+++ src/test/org/github/mlauper/PancakeTest.java
@@ -1,8 +1,13 @@
 package org.github.mlauper;
 
+import github.mlauper.Pancake;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class PancakeTest extends TestCase {
     public PancakeTest( String testName )
@@ -14,9 +19,90 @@ public class PancakeTest extends TestCase {
         return new TestSuite( PancakeTest.class );
     }
 
-    public void testPancake()
-    {
+    private LinkedList<Integer> pancakeList1 = new LinkedList<>(Arrays.asList(2, 1, 4, 3, 6, 5));
 
+    public void dummy()
+    {
         assertTrue( true );
+    }
+
+    public void testIfPancakeCanBeInitialized()
+    {
+        Pancake pancake = new Pancake(pancakeList1);
+        assertNotNull(pancake);
+    }
+
+    public void testIfPancakeCanBePrinted()
+    {
+        Pancake pancake = new Pancake(pancakeList1);
+        try {
+            pancake.printPancakes();
+        } catch (Exception ex){
+            fail(ex.getMessage());
+        }
+    }
+
+    public void testIfPancakeFlipsCanBeCalculated()
+    {
+        LinkedList<Integer> orderedPancakeList = new LinkedList<>(Arrays.asList(1,3,2,4,5,6));
+        Pancake pancake = new Pancake(orderedPancakeList);
+
+        pancake.calculateFlips();
+        pancake.printPancakes();
+    }
+
+    public void testIfPancakeFlipsCanBeCalculatedForAlreadyOrderedPancakes()
+    {
+        LinkedList<Integer> orderedPancakeList = new LinkedList<>(Arrays.asList(1,2,3,4,5,6));
+        Pancake pancake = new Pancake(orderedPancakeList);
+
+        pancake.calculateFlips();
+        pancake.printPancakes();
+    }
+
+    public void testIfPancakeChecksOrderingCorrectOnOrderedPancakes(){
+        LinkedList<Integer> orderedPancakeList = new LinkedList<>(Arrays.asList(1,2,3,4,5,6));
+        Pancake pancake = new Pancake(orderedPancakeList);
+        assertTrue(pancake.isPancakeOrdered());
+    }
+
+    public void testIfPancakeChecksOrderingCorrectOnUnorderedPancakes(){
+        LinkedList<Integer> orderedPancakeList = new LinkedList<>(Arrays.asList(1,2,3,6,5,4));
+        Pancake pancake = new Pancake(orderedPancakeList);
+        assertFalse(pancake.isPancakeOrdered());
+    }
+
+    public void testIfPancakesAreReturned(){
+        LinkedList<Integer> pancakes = new LinkedList<>(Arrays.asList(1,2,3,6,5,4));
+        Pancake pancake = new Pancake(pancakes);
+
+        LinkedList<Integer> pancakes2 = pancake.getPancakes();
+
+        assertEquals(pancakes,pancakes2);
+    }
+
+    public void testIfPancakesAreFlipped(){
+        LinkedList<Integer> pancakes = new LinkedList<>(Arrays.asList(1,2,3,6,5,4));
+        LinkedList<Integer> flipedPancakes = new LinkedList<>(Arrays.asList(1,2,3,4,5,6));
+        Pancake pancake = new Pancake(pancakes);
+
+        pancake.flip(3);
+
+        assertEquals(flipedPancakes, pancake.getPancakes());
+    }
+
+    public void testIfChildStatesCanBeRetrieved(){
+        LinkedList<Integer> pancakes = new LinkedList<>(Arrays.asList(1,2,3,4,5));
+        Pancake pancake = new Pancake(pancakes);
+
+        LinkedList<LinkedList<Integer>> childPancakes =
+                new LinkedList<>(Arrays.asList(
+                        new LinkedList<>(Arrays.asList(1,5,4,3,2)),
+                        new LinkedList<>(Arrays.asList(1,2,5,4,3)),
+                        new LinkedList<>(Arrays.asList(1,2,3,5,4))
+                ));
+
+        assertEquals(childPancakes, pancake.getChildStates());
+        System.out.println(pancake.getChildStates());
     }
 }
