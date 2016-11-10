a6dc9b84e37166920822fc92d0e6f20949618bdc
mLauper <laupm4@bfh.ch>
2016-11-10 19:37:43 +0100

    Change SumAll

--- src/SumAll.java
+++ src/SumAll.java
@@ -1,8 +1,6 @@
 import mpi.MPI;
 
-import java.util.ArrayList;
-
-public class Ring {
+public class SumAll {
     public static void main(String[] args) {
 
         MPI.Init(args);
@@ -15,40 +13,11 @@ public class Ring {
 
         startTime = System.nanoTime();
 
-        mpi.Request request;
-        if (rank == 0) {
-            // first rank, receive from last rank
-            System.out.format("Rank %d: Receive from %d, Send to %d\n", rank, numOfRanks-1, rank+1);
-            request = MPI.COMM_WORLD.Irecv(inBuffer, 0, bufferSize, MPI.INT, numOfRanks-1, 1);
-            MPI.COMM_WORLD.Send(outBuffer, 0, bufferSize, MPI.INT, rank+1, 1);
-            try {
-                request.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        } else if (rank == (numOfRanks-1)) {
-            // Last rank, send to rank 0
-            System.out.format("Rank %d: Receive from %d, Send to %d\n", rank, rank-1, 0);
-            request = MPI.COMM_WORLD.Irecv(inBuffer, 0, bufferSize, MPI.INT, rank-1, 1);
-            MPI.COMM_WORLD.Send(outBuffer, 0, bufferSize, MPI.INT, 0, 1);
-            try {
-                request.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        } else {
-            // other ranks
-            System.out.printf("Rank %d: Receive from %d, Send to %d\n", rank, rank-1, rank+1);
-            request = MPI.COMM_WORLD.Irecv(inBuffer, 0, bufferSize, MPI.INT, rank-1, 1);
-            MPI.COMM_WORLD.Send(outBuffer, 0, bufferSize, MPI.INT, rank+1, 1);
-            try {
-                request.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        outBuffer[0] = rank;
+        MPI.COMM_WORLD.Scan(outBuffer, 0, inBuffer, 0, bufferSize, MPI.INT, MPI.SUM);
+
         endTime = System.nanoTime();
-        System.out.format("Finished, Rank: %d, Measured Time: %d ms\n", rank, (endTime-startTime)/1000/1000);
+        System.out.format("Finished, Rank: %d, Measured Time: %d ms, Calculated Sum: %d\n", rank, (endTime-startTime)/1000/1000, inBuffer[0]);
 
 
         MPI.Finalize();
