aed4e8c4b6a8864af43abe018a51c13225039f88
mLauper <laupm4@bfh.ch>
2016-11-10 19:37:17 +0100

    Change CustomGroupOperation

--- src/CustomGroupOperation.java
+++ src/CustomGroupOperation.java
@@ -1,6 +1,9 @@
+import mpi.Datatype;
 import mpi.MPI;
+import mpi.Op;
+import mpi.User_function;
 
-public class SumAll {
+public class CustomGroupOperation {
     public static void main(String[] args) {
 
         MPI.Init(args);
@@ -14,12 +17,20 @@ public class SumAll {
         startTime = System.nanoTime();
 
         outBuffer[0] = rank;
-        MPI.COMM_WORLD.Scan(outBuffer, 0, inBuffer, 0, bufferSize, MPI.INT, MPI.SUM);
+        MPI.COMM_WORLD.Scan(outBuffer, 0, inBuffer, 0, bufferSize, MPI.INT, new Op(new MyOp(), false));
 
         endTime = System.nanoTime();
         System.out.format("Finished, Rank: %d, Measured Time: %d ms, Calculated Sum: %d\n", rank, (endTime-startTime)/1000/1000, inBuffer[0]);
 
 
         MPI.Finalize();
+    }
+
+    static class MyOp extends User_function {
+        @Override
+        public void Call(Object inObj, int inOffset, Object outObj, int outOffset, int cnt, Datatype arg5){
+
+            ((int[])outObj)[0] = 42;
+        }
     }
 }
